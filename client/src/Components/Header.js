37578816be18573e9c45f93befd81c0fe2ed6ad8
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-02-12 21:37:38 -0500

    [Header] - Added Header

---
+++ client/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <h1>Collab</h1>
+        <p>There's a partner for that</p>
+      </header>
+    </div>
+  )
+}
