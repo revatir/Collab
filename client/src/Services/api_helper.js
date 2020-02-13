4dc564910a752fbffef3db5fe21d93619698e531
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-02-12 21:41:12 -0500

    Added Services

---
+++ client/src/Services/api_helper.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: "http://localhost:3000"
+})
+
+// AUTH
+
+// LOGIN
+export const loginUser = async (loginData) => {
+  const resp = await api.post('/auth/login', loginData);
+  console.log(resp);
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
+  localStorage.setItem('authToken', resp.data.auth_token);
+  localStorage.setItem('name', resp.data.user.name);
+  localStorage.setItem('email', resp.data.user.email);
+  return resp.data.user;
+}
+
+// REGISTER
+export const registerUser = async (registerData) => {
+  // try {
+  const resp = await api.post('/signup', registerData);
+  console.log(resp)
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
+  localStorage.setItem('authToken', resp.data.auth_token);
+  localStorage.setItem('name', resp.data.user.name);
+  localStorage.setItem('email', resp.data.user.email);
+  return resp.data.user;
+  // } catch (e) {
+  //   console.log(e.response);
+  //   if (e.response.status === 422) {
+  //     return { errorMessage: "Email is already associated with a user, please login to continue" }
+  //   }
+  // }
+}
+
+// VERIFY USER
+export const verifyUser = () => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+  }
+}
+
+// COMPANY CRUD
+// GET ALL COMPANIES
+export const indexAllCompanies = async () => {
+  const resp = await api.get('/companies');
+  return resp.data;
+}
+
+// GET USER COMPANY
+export const showCompany = async (id) => {
+  const resp = await api.get(`/users/${id}/companies`);
+  return resp.data;
+}
+
+//POST COMPANY
+export const createCompany = async (postData) => {
+  const resp = await api.post('/users/:user_id/company', postData);
+  return resp.data;
+}
+
+// UPDATE COMPANY
+export const putCompany = async (id, postData) => {
+  const resp = await api.put(`/todos/${id}`, postData);
+  const todo = { id: id, title: resp.data.data }
+  return todo;
+}
+
+// REVIEW CRUD
+
+// GET REVIEWS
+export const showReviews = async (id) => {
+  const resp = await api.get(`/users/${id}/companies/1/reviews`);
+  return resp.data;
+}
+
+//POST REVIEWS
+export const createReview = async (id, postData) => {
+  const resp = await api.post(`/users/${id}/companies/1/reviews`, postData);
+  return resp.data;
+}
