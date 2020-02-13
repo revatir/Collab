be29c3ea0cad47d0039c74cf1dea4b811925c88f
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-02-13 14:00:38 -0500

    [Response] - Updated response methods to return new user object

--- server/app/controllers/concerns/response.rb
+++ server/app/controllers/concerns/response.rb
@@ -1,4 +1,7 @@
 module Response
+  def create_response(object, object2, status = :ok)
+    render json: object, json: object2, status: status
+  end
   def json_response(object, status = :ok)
     render json: object, status: status
   end
