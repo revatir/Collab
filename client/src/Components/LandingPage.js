8684550ab9e21c90b0f79972a27f22a210451108
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-02-20 09:12:38 -0500

    [LandingPage] cleaned up code, added intro text, and added class names

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function LandingPage(props) {
   return (
-    <div>
-      Welcome to Collab
-    </div>
+    <div className="landing-page-wrapper">
+      <h4>Welcome to Collab!</h4>
+      <p>Create a network. Find an opportunity. Make a connection.</p>
+      <p>Collab is here to help your startup connect with and support others in your community.</p>
+      <ul>
+        {props.currentUser === true &&
+          <div>
+            <Link to="/explore"><li>Explore</li></Link>
+          </div>
+        }
+        {props.currentUser === false &&
+          <div>
+            <Link to="/login"><li>Login</li></Link>
+            <Link to="/register"><li>Register</li></Link>
+          </div>
+        }
+      </ul>
+    </div >
   )
 }
