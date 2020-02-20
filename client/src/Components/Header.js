ed37b1d62203efa24f3f7e893ed42a284b2854c6
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-02-20 09:12:12 -0500

    [Header] cleaned up code and added class names

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
+    <div className="header-wrapper">
       <header>
         <h1>Collab</h1>
         <p>There's a partner for that</p>
